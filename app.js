a3ad01db30f3f56923778ad30eb62bded2be7973
HuHp <106983123+nghuuhiep@users.noreply.github.com>
2024-11-17 22:16:54 +0700

    Update app.js

--- app.js
+++ app.js
@@ -7,27 +7,58 @@ let prev = document.getElementById('prev');
 let active = 2;
 function loadShow() {
     let stt = 0;
-    items[active].style.transform = `none`;
-    items[active].style.zIndex = 1;
-    items[active].style.filter = 'none';
-    items[active].style.opacity = 1;
     window.isDevice = (/android|webos|iphone|ipad|ipod|blackberry|iemobile|opera mini/i.test(((navigator.userAgent || navigator.vendor || window.opera)).toLowerCase()));
     let mobile = window.isDevice;
     for (let i = 0; i < items.length; i++){
         let main = items[i].style;
         main.height = mobile ? "30vh" : "320px"
         main.width = mobile ? "33.5vw" : "200px"
-        main.transform = "none"
     }
     let show = mobile ? 2 : 4;
-    let distance = mobile ? 50 : 100
+    let marCenter = mobile ? 20 : 100
+    let marSide = mobile ? 50 : 80
+    items[active-2].style.transform = `none`;
+    items[active-2].style.zIndex = 1;
+    items[active-2].style.filter = 'none';
+    items[active-2].style.opacity = 1;
+    items[active-2].style.transform = mobile ? "scale(1.3)" : "scale(1.1)";
+    items[active-2].style.fontSize = mobile ? "0.9vh" : "2vh";
+    items[active-2].style.padding = mobile ? "20px" : "50px"
+
+    items[active-1].style.transform = `none`;
+    items[active-1].style.zIndex = 1;
+    items[active-1].style.filter = 'none';
+    items[active-1].style.opacity = 1;
+    items[active-1].style.transform = mobile ? "scale(1.3)" : "scale(1.1)";
+    items[active-1].style.fontSize = mobile ? "0.9vh" : "2vh";
+    items[active-1].style.padding = mobile ? "20px" : "50px"
+    
+    items[active].style.transform = `none`;
+    items[active].style.zIndex = 1;
+    items[active].style.filter = 'none';
+    items[active].style.opacity = 1;
     items[active].style.transform = mobile ? "scale(1.3)" : "scale(1.1)";
     items[active].style.fontSize = mobile ? "0.9vh" : "2vh";
     items[active].style.padding = mobile ? "20px" : "50px"
+    
+    items[active+1].style.transform = `none`;
+    items[active+1].style.zIndex = 1;
+    items[active+1].style.filter = 'none';
+    items[active+1].style.opacity = 1;
+    items[active+1].style.transform = mobile ? "scale(1.3)" : "scale(1.1)";
+    items[active+1].style.fontSize = mobile ? "0.9vh" : "2vh";
+    items[active+1].style.padding = mobile ? "20px" : "50px"
+
+    items[active+2].style.transform = `none`;
+    items[active+2].style.zIndex = 1;
+    items[active+2].style.filter = 'none';
+    items[active+2].style.opacity = 1;
+    items[active+2].style.transform = mobile ? "scale(1.3)" : "scale(1.1)";
+    items[active+2].style.fontSize = mobile ? "0.9vh" : "2vh";
+    items[active+2].style.padding = mobile ? "20px" : "50px"
     for (var i = active + 1; i < items.length; i++) {
         stt++;
-        items[i].style.transform = `translateX(${distance + (80 * stt)}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(${-0.55*stt}deg`;
-        // items[i].style.transform = `translateX(${100 + (80 * stt)}px) scale(${1 - 0.2 * stt})`;
+        items[i].style.transform = `translateX(${marCenter + (marSide * stt)}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(${-0.55*stt}deg`;
         items[i].style.zIndex = -stt;
         items[i].style.filter = `blur(${5 * stt}px)`;
         items[i].style.opacity = stt > show ? 0 : 0.6;
@@ -35,8 +66,7 @@ function loadShow() {
     stt = 0;
     for (var i = active - 1; i >= 0; i--) {
         stt++;
-        items[i].style.transform = `translateX(${-distance + (-80 * stt)}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(${0.55*stt}deg)`;
-        // items[i].style.transform = `translateX(${-100 + (-80 * stt)}px) scale(${1 - 0.2 * stt})`;
+        items[i].style.transform = `translateX(${-marCenter + (-marSide * stt)}px) scale(${1 - 0.2 * stt}) perspective(16px) rotateY(${0.55*stt}deg)`;
         items[i].style.zIndex = -stt;
         items[i].style.filter = `blur(${5 * stt}px)`;
         items[i].style.opacity = stt > show ? 0 : 0.6;
